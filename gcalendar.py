4f66e1c90792b1a0a0251d20647af339e6ff4df2
CurbsideProph3t <rodriguezmoreno.marcos@gmail.com>
2019-11-14 14:09:53 +0100

    Fix Message

--- gcalendar.py
+++ gcalendar.py
@@ -48,16 +48,16 @@ def get_calendar(cid):
 
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
+    print('Getting the upcoming 3 events')
     events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
+                                        maxResults=3, singleEvents=True,
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
 
     if not events:
         return ('No upcoming events found.')
     for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
+        start = str(event['start'].get('dateTime'))
         bot.send_chat_action(cid, 'typing')
         time.sleep(0.2)
-        bot.send_message(cid, str((start, event['summary'])))
+        bot.send_message(cid, str((start.split("T"), str(event['summary']))))
